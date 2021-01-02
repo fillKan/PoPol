554251e0b5e83aaf4f3ab97d94870c09386fe5ad
kimbokChi <han15050424@gmail.com>
2021-01-02 15:40:04 +0900

    원을 그리면서 이동하는 물고기들을 추가한다
    물고기들의 이러한 행동은 SinCurve스크립트를 통해 구현된다. 물고기들은 원을 그리면서
    sin곡선에 따라 스케일이 변경된다.

---
+++ Assets/Script/SinCurve.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class SinCurve : MonoBehaviour
+{
+    [Range(0.1f, 2f)]
+    [SerializeField] private float _CurveScale;
+    [SerializeField] private float _Period;
+    [SerializeField] private float _ScaleMultiply;
+
+    private Vector3 _OriginPosition;
+    private Vector3 _OriginScale;
+
+    private void Start()
+    {
+        _OriginPosition = transform.localPosition;
+        _OriginScale = transform.localScale;
+    }
+
+    private void Update()
+    {
+        float sin = Mathf.Sin(Mathf.PI * Time.time * _Period) * _CurveScale;
+        float cos = Mathf.Cos(Mathf.PI * Time.time * _Period) * _CurveScale;
+
+        transform.localPosition = _OriginPosition + Vector3.up * sin + Vector3.right * cos;
+
+        transform.localScale = (Vector3.one * sin * _ScaleMultiply); // + (Vector3.right * cos * _ScaleMultiply);
+    }
+}
