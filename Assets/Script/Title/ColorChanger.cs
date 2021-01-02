bfb32a33754240fc311d08ae05a28a08424725ce
kimbokChi <han15050424@gmail.com>
2021-01-02 22:47:51 +0900

    카메라의 배경 색상을 전환하는 스크립트를 추가한다
    추가한 스크립트의 이름은 ColorChaneger이다. 적절한 사용처를 못찾았다..

---
+++ Assets/Script/Title/ColorChanger.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorChanger : MonoBehaviour
+{
+    [SerializeField] private float _Duration;
+    [SerializeField] private float _ChangeTime;
+
+    [Space()]
+    [SerializeField] private Color[] _Colors;
+
+    private int _ColorIndex;
+
+    private void Awake()
+    {
+        _ColorIndex = 0;
+
+        StartCoroutine(ColorChange());
+    }
+    private IEnumerator ColorChange()
+    {
+        while (enabled)
+        {
+            Color cameraColor = Camera.main.backgroundColor;
+
+            for (float i = 0; i < _ChangeTime; i += Time.deltaTime)
+            {
+                float ratio = i / _ChangeTime;
+
+                Camera.main.backgroundColor = Color.Lerp(cameraColor, _Colors[_ColorIndex], ratio);
+
+                yield return null;
+            }
+            if (++_ColorIndex >= _Colors.Length)
+            {
+                _ColorIndex = 0;
+            }
+            yield return new WaitForSeconds(_Duration);
+        }
+    }
+}
