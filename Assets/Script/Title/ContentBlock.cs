4d9c29e6a34a80fc62cdf5c1193f89971a2b0b1c
kimbokChi <han15050424@gmail.com>
2021-01-03 12:11:10 +0900

    메인화면 씬을 구성한다
    메인화면 씬에는 다른 씬들로 이동할 수 있는 기능을 제공할 계획이다.
    이 기능은 메인화면에있는 카드를 선택하여 이동할 수 있다.

---
+++ Assets/Script/Title/ContentBlock.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class ContentBlock : MonoBehaviour, IPointerDownHandler
+{
+    [SerializeField] private float _AnimationTime;
+    
+    private float _StartScale;
+    private bool _IsOpend;
+
+    private IEnumerator _Animation;
+
+    private void Awake()
+    {
+        _IsOpend = false;
+        _StartScale = transform.localScale.x;
+    }
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        if (_Animation != null)
+        {
+            StopCoroutine(_Animation);
+        }
+        StartCoroutine(_Animation = Animation(!_IsOpend));
+    }
+    private IEnumerator Animation(bool open)
+    {
+        _IsOpend = open;
+        Vector2 targetScale;
+
+        if (open)
+        {
+            targetScale = Vector2.one;
+        }
+        else
+        {
+            targetScale = _StartScale * Vector2.one;
+        }
+        for (float i = 0f; i < _AnimationTime; i += Time.deltaTime)
+        {
+            i = Mathf.Min(1f, i);
+
+            float ratio = i / _AnimationTime;
+            transform.localScale = Vector2.Lerp(transform.localScale, targetScale, ratio);
+
+            LayoutRebuilder.ForceRebuildLayoutImmediate(transform.parent.GetComponent<RectTransform>());
+
+            yield return null;
+        }
+        _Animation = null;
+    }
+}
