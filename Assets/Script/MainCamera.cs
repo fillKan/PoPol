9b8699db6205873f728d9768fbd1d244ee563f39
kimbokChi <han15050424@gmail.com>
2021-01-11 19:26:57 +0900

    MainCamera의 카메라 이동 기능을 추가한다
    이 카메라 이동 기능은 SceneDirector스크립트에서 _MoveTarget이 null일 경우 제공하던
    기능으로, 이 기능을 MainCamera스크립트로 이관한다.
    
    동시의 이 기능을 강화하였는데, 카메라 흔들림이 실행중인 도중에도 이동이 가능하도록
    카메라 자신이 움직이는 것이 아닌 자신의 부모 오브젝트를 움직여서 실행된다.

--- Assets/Script/MainCamera.cs
+++ Assets/Script/MainCamera.cs
@@ -1,10 +1,10 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class MainCamera : Singleton<MainCamera>
 {
     private Coroutine _ShakeRoutine;
+    private Coroutine  _MoveRoutine;
 
     private void Awake()
     {
@@ -14,11 +14,27 @@ public class MainCamera : Singleton<MainCamera>
             transform.localPosition = Vector2.zero;
             transform.Translate(0, 0, -10f);
         };
+        _MoveRoutine = new Coroutine(this);
     }
+
     public void Shake(float power, float time)
     {
         _ShakeRoutine.StartRoutine(EShake(power, time));
     }
+    public void Move(float time, Vector2 start, Vector2 goal, System.Action overAction = null)
+    {
+        transform.parent.position = start;
+        _MoveRoutine.StartRoutine(EMove(time, goal));
+
+        void OverAction()
+        {
+            overAction?.Invoke();
+            _MoveRoutine.RoutineStopEvent -= OverAction;
+        }
+        _MoveRoutine.RoutineStopEvent += OverAction;
+    }
+
+    // =================== IEnumator =================== //
     private IEnumerator EShake(float power, float time)
     {
         for (float i = 0; i < time; i += Time.deltaTime)
@@ -32,5 +48,20 @@ public class MainCamera : Singleton<MainCamera>
             yield return null;
         }
         _ShakeRoutine.FinshRoutine();
+    }
+    private IEnumerator EMove(float time, Vector2 position)
+    {
+        Transform parent = transform.parent;
+
+        for (float i = 0f; i < time; i += Time.deltaTime)
+        {
+            float ratio = Mathf.Min(i, time) / time;
+
+            parent.position = Vector2.Lerp(transform.position, position, ratio);
+            parent.Translate(0, 0, -10f);
+
+            yield return null;
+        }
+        _MoveRoutine.FinshRoutine();
     }
 }
