dcd7aad188dc420951236d9b7f70480a3789d19e
kimbokChi <han15050424@gmail.com>
2021-01-13 09:26:40 +0900

    인트로 씬을 구성하고, 인트로 씬의 연출을 설정하는 스크립트를 추가한다
    인트로 씬의 연출은 IntroScene스크립트를 통해 구현된다. 인트로 영상이 재생되고,
    이후에 화면에 글자가 나타났다가 사라진다.

---
+++ Assets/Script/Video/IntroScene.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class IntroScene : MonoBehaviour
+{
+    [SerializeField] private VideoPlayer _Video;
+    [SerializeField] private AlphaFader _AlphaFader;
+
+    private void Awake()
+    {
+        MainCamera.Instance.SetColor(Color.black);
+    }
+    private void Start()
+    {
+        _AlphaFader.SetAlpha(0f);
+
+        _Video.loopPointReached += VideoPlayCompleted;
+    }
+    private void VideoPlayCompleted(VideoPlayer source)
+    {
+        _Video.targetCameraAlpha = 0f;
+        StartCoroutine(Direction());
+    }
+    private IEnumerator Direction()
+    {
+        float videFadeTime = 1.5f;
+        for (float i = 0f; i < videFadeTime; i += Time.deltaTime)
+        {
+            float ratio = Mathf.Min(i / videFadeTime, 1f);
+
+            _Video.targetCameraAlpha = Mathf.Lerp(1f, 0f, ratio);
+            yield return null;
+        }
+        yield return new WaitForSeconds(0.5f);
+        _AlphaFader.AlphaFadeRegular(1f, 3f);
+
+        yield return new WaitForSeconds(3.5f);
+        _AlphaFader.AlphaFadeRegular(0f, 2f);
+    }
+}
