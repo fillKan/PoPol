b8c0d5715f26dd5cb4a7ce11f1ae75b0aa90e19d
kimbokChi <han15050424@gmail.com>
2021-01-14 13:44:02 +0900

    자막들을 스크립터블 오브젝트로 저장한다
    스크립터블 오브젝트, SubtitleSet을 통해 자막들을 저장하고,
    모든 자막들을 배열로 불러오는 기능과, 자막을 순서대로 제공하는 두 기능을 제공한다.

---
+++ Assets/Script/Video/SubtitleSet.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "SubtitleSet", menuName = "Subtitle/SubtitleSet")]
+public class SubtitleSet : ScriptableObject
+{
+    private int _Index = 0;
+
+    [TextArea(3, 3)]
+    [SerializeField] private string[] _Subtitles;
+
+    public string[] SubtitleAll()
+    {
+        return _Subtitles;
+    }
+    public string NextSubtitle()
+    {
+        if (_Index < _Subtitles.Length)
+        {
+            return _Subtitles[_Index++];
+        }
+        else
+            return string.Empty;
+    }
+}
