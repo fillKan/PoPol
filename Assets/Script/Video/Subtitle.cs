3cc1b829055f4f2999ca927bd97ffcd2937771bd
kimbokChi <han15050424@gmail.com>
2021-01-14 09:04:53 +0900

    자막 기능을 제공하는 Subtitle스크립트를 추가한다
    자막을 블록으로 구분하고, 그 자막을 블록마다 출력하는 기능을 제공하는 스크립트이다.
    현재는 자막을 출력하는 기능을 보기위해서 자막을 한번만 출력하도록 되어있다.

---
+++ Assets/Script/Video/Subtitle.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * BGRect.w, BGRect.h
+ *  |_____________  |__
+ *                ↓    ↓
+ * 1 : 1.1 : 1.5 : 1.8 : 2
+ * ↑    ↑     ↑____________
+ * |    |_______           |
+ * |            |          |
+ * FontSize, FontRect.w, FontRect.h
+ */
+public class Subtitle : MonoBehaviour
+{
+    [SerializeField] private TMPro.TextMeshProUGUI _Text;
+    [SerializeField] private RectTransform _BackGroundRect;
+
+    [Space(10f)][TextArea(2, 4)]
+    [SerializeField] private string[] _Subtitles;
+
+    private IEnumerator _SubtitleCollection;
+
+    private void Awake()
+    {
+        _SubtitleCollection = _Subtitles.GetEnumerator();
+        _SubtitleCollection.MoveNext();
+
+        _Text.text = (string)_SubtitleCollection.Current;
+
+        int blankCount = _Text.text.Split(' ').Length - 1;
+
+        _Text.rectTransform.sizeDelta = new Vector2(_Text.fontSize * _Text.text.Length * 1.1f, _Text.fontSize * 1.5f);
+        _BackGroundRect.sizeDelta = new Vector2(_Text.fontSize * _Text.text.Length * 1.1f, _Text.fontSize * 2f);
+    }
+}
