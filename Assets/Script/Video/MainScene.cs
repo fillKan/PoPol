b697b3216ae2d437787f86b2cd46c2005fb55de0
kimbokChi <han15050424@gmail.com>
2021-01-16 15:27:51 +0900

    메인 씬에서 다음 씬으로 이동하기위한 연출을 일부 구현한다
    이는 MainScene스크립트를 통해서 구현한다.
    
    ContentBlock을 선택한 상태에서 다시 선택하면, 그 블럭은 원상태로 돌아가고 이후 배경을 비롯한
    주변의 요소들을 투명해지게한다.

--- Assets/Script/Video/MainScene.cs
+++ Assets/Script/Video/MainScene.cs
@@ -1,10 +1,48 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MainScene : MonoBehaviour
 {
     [SerializeField] private Animator _Animator;
+    [SerializeField] private AlphaFader _AlphaFader;
+
+    private void Awake()
+    {
+        var sections = FindObjectsOfType<ContentBlock>();
+
+        for (int i = 0; i < sections.Length; i++)
+        {
+            sections[i].SelectedClickEvent += section => 
+            {
+                // MainScene은 섹션 블럭들을 포함하는 캔버스이기 때문에 가능한 코드
+                section.transform.parent = transform;
+                section.PlayAnimation(ContentBlock.AnimationType.Default);
+                
+                StartCoroutine(Enumerator(section));
+            };
+        }
+    }
+    private IEnumerator Enumerator(ContentBlock content)
+    {
+        content.TryGetComponent(out RectTransform contentRect);
+
+        yield return new WaitForSeconds(ContentBlock.AnimationTime);
+        _AlphaFader.AlphaFade (0f, ContentBlock.AnimationTime);
+
+        Vector2 center = new Vector2(960.0f, -540.0f);
+        for (float i = 0f; i < 1f; i += Time.deltaTime)
+        {
+            float ratio = Mathf.Min(i / 1f, 1f);
+
+            contentRect.anchoredPosition =
+                Vector2.Lerp(contentRect.anchoredPosition, center, ratio);
+
+            yield return null;
+        }
+        contentRect.anchoredPosition = center;
+    }
 
     public void AnimatorDisable()
     {
