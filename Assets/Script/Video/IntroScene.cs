9579b7b8f63484afaf796d75437cede26d4e2e0e
kimbokChi <han15050424@gmail.com>
2021-01-18 22:14:39 +0900

    인트로 씬에서 사용하는 브금을 추가한다

--- Assets/Script/Video/IntroScene.cs
+++ Assets/Script/Video/IntroScene.cs
@@ -8,6 +8,9 @@ public class IntroScene : MonoBehaviour
 {
     public const int MainSceneBuildIndex = 5;
 
+    [SerializeField] private Audio _Audio;
+    [SerializeField] private float _Volume;
+
     [Header("Intro Direction")]
     [SerializeField] private VideoPlayer _Video;
     [SerializeField] private AlphaFader _AlphaFader;
@@ -77,10 +80,15 @@ public class IntroScene : MonoBehaviour
 
         yield return new WaitForSeconds(3.0f);
         _Orientation.SetActive(true);
+
+        _Audio.VoulmeCotrol(4.5f, _Volume);
+        _Audio.Play();
     }
 
     private IEnumerator MainSceneLoad(float animTime)
     {
+        _Audio.VoulmeCotrol(animTime * 0.9f, 0f);
+
         int hash = _Animator.GetParameter(0).nameHash;
         _Animator.SetBool(hash, true);
 
