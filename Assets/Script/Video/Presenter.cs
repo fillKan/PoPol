c7c49a8eb01fa0d8eee7b9bb97d77add7e3981ef
kimbokChi <han15050424@gmail.com>
2021-01-18 12:59:37 +0900

    이미지들을 한장 한장 넘길 수 있게 해주는 스크립트를 추가한다
    추가한 스크립트의 이름은 Presenter이다.

---
+++ Assets/Script/Video/Presenter.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Presenter : MonoBehaviour
+{
+    [SerializeField] private Image _Image;
+
+    [Header("Presentation Sprite")]
+    [SerializeField] private Sprite[] _Pages;
+    private int _Index;
+
+    private void Awake()
+    {
+        _Index = 0;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (_Index < _Pages.Length)
+            {
+                _Image.sprite = _Pages[_Index++];
+            }
+        }
+    }
+}
